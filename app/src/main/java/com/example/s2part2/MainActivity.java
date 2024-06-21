4639643e1cdf65a230f46b22a42486b82d29147b
jishan <imranislamjishan80@gmail.com>
2024-06-22 00:55:22 +0600

    navigation view add

--- app/src/main/java/com/example/s2part2/MainActivity.java
+++ app/src/main/java/com/example/s2part2/MainActivity.java
@@ -1,21 +1,47 @@
 package com.example.s2part2;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    MaterialToolbar materialToolbar;
+    FrameLayout frameLaout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.dowerLayout);
+        materialToolbar = findViewById(R.id.materialToolbar);
+        frameLaout = findViewById(R.id.frameLaout);
+        navigationView = findViewById(R.id.navigationView);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout ,
+                materialToolbar,
+                R.string.drawer_close,
+                R.string.drawer_open
+                );
+
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
 
     }
 
-
+    //------------------ On create End here ----------------------
 
 
 
